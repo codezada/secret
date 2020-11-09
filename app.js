168a72b50235fecf3511fd7d6002a900c3192682
Hafizur Rahman <hafizurjay@gmail.com>
2020-11-09 11:09:43 +0530

    Level 2 - Encrypt database using mongoose-ecryption package

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-//jshint esversion:6
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption')
+
 
 const app = express();
 
@@ -11,10 +13,12 @@ app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true,useUnifiedTopology:true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email : String,
     password : String
-}
+});
+const secret = "ThisIsMySecretKey";
+userSchema.plugin(encrypt,{secret:secret,encryptedFields: ['password']})
 
 const User = new mongoose.model('User',userSchema)
 
