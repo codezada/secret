d04f9d7b7a4bcafcb5160f883fb8d183a61d968d
Hafizur Rahman <hafizurjay@gmail.com>
2020-11-15 11:49:46 +0530

    Add env variale using dotenv package

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 
+
+//use dotenv to secure our api key 
+
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption')
+
 
 
 const app = express();
@@ -13,12 +19,19 @@ app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true,useUnifiedTopology:true});
 
+console.log(process.env.SECRET);
+
+
 const userSchema = new mongoose.Schema({
     email : String,
     password : String
 });
-const secret = "ThisIsMySecretKey";
-userSchema.plugin(encrypt,{secret:secret,encryptedFields: ['password']})
+
+//acess environment variable of .env file iinside app.js
+
+const secret = process.env.SECRET;
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields: ['password']})
 
 const User = new mongoose.model('User',userSchema)
 
