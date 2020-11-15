ce8a588c51694e585736bb1f3accded6a676cf2e
Hafizur Rahman <hafizurjay@gmail.com>
2020-11-15 15:53:13 +0530

    Level 4 Security . using bcrypt to secure password using saltRounds.

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 
 
-//use dotenv to secure our api key 
-
-require('dotenv').config();
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption')
+const bcrypt= require('bcrypt');
+const saltRounds = 10;
+
 
 
 
@@ -27,11 +25,7 @@ const userSchema = new mongoose.Schema({
     password : String
 });
 
-//acess environment variable of .env file iinside app.js
 
-const secret = process.env.SECRET;
-
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields: ['password']})
 
 const User = new mongoose.model('User',userSchema)
 
@@ -48,31 +42,44 @@ app.get("/login",function(req,res){
 });
 
 app.post("/register",function(req,res){
-    const newUser = new User({
-        email : req.body.username,
-        password : req.body.password
-    })
 
-    newUser.save(function(err){
-        if(err){
-            console.log(err)
-        }else{
-            res.render("secrets")
-        }
-    })
+
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        
+
+
+        const newUser = new User({
+            email : req.body.username,
+            password : hash
+        })
+    
+        newUser.save(function(err){
+            if(err){
+                console.log(err)
+            }else{
+                res.render("secrets")
+            }
+        })
+    });
+
 })
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = (req.body.password);
 
     User.findOne({email : username},function(err,foundUser){
         if(err){
             console.log(err)
-        }else if(foundUser.password===password){
-            console.log(foundUser.email)
+        }else if(foundUser){
+
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+             console.log(foundUser.email)
             console.log(foundUser.password)
-            res.render("secrets")
+            res.render("secrets")  
+            });
+            
         }else{
             console.log("Sorry your password is incorrect!")
         }
